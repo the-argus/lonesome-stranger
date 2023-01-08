2b7addc099f796e34172b276d182ee17e8d7c228
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-08 11:12:49 -0600

    almost complete dungeonsheets build

--- flake.nix
+++ flake.nix
@@ -11,7 +11,44 @@
       "aarch64-linux"
     ];
     genSystems = nixpkgs.lib.genAttrs supportedSystems;
-    pkgs = genSystems (system: import nixpkgs {inherit system;});
+
+    overlays = [
+      (_: super: {
+        python310Packages =
+          super.python310Packages
+          // {
+            fdfgen = super.python310Packages.buildPythonPackage rec {
+              pname = "fdfgen";
+              version = "0.16.1";
+              src = super.python310Packages.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-DtpjtesIEIQ92YQ3d4cC+zlt71EJDOigEYblI04kf44=";
+              };
+            };
+            EBookLib = super.python310Packages.buildPythonPackage rec {
+              pname = "EbookLib";
+              version = "0.18";
+              propagatedBuildInputs = with super.python310Packages; [six lxml];
+              src = super.python310Packages.fetchPypi {
+                inherit pname version;
+                sha256 = "sha256-OFYmQ6e8lNm/VumTC0kn5Ok7XR0JF/aXpkVNtaHBpTM=";
+              };
+            };
+            certifi = super.python310Packages.buildPythonPackage rec {
+              pname = "certifi";
+              version = "2018.01.18";
+              src = super.fetchFromGitHub {
+                owner = "certifi";
+                repo = "python-certifi";
+                rev = version;
+                sha256 = "sha256-GKeqJGXOcqqH0PYXlJOSAW9NoKL+BXzA8ZvqC1cMW4A=";
+              };
+            };
+          };
+      })
+    ];
+
+    pkgs = genSystems (system: import nixpkgs {inherit system overlays;});
   in {
     packages = genSystems (system: rec {
       # the actual class documentation pdf
@@ -20,11 +57,12 @@
       default = class;
     });
 
-    devShell = genSystems (system: {
-      packages = with pkgs.${system}; [
-        (python310.withPackages
-          (_: [pkgs.${system}.callPackage ./nix/dungeonsheets.nix] {}))
-      ];
-    });
+    devShell = genSystems (system:
+      pkgs.${system}.mkShell {
+        packages = with pkgs.${system}; [
+          (python310.withPackages
+            (_: [(pkgs.${system}.callPackage ./nix/dungeonsheets.nix {})]))
+        ];
+      });
   };
 }
--- nix/dungeonsheets.nix
+++ nix/dungeonsheets.nix
@@ -1,16 +1,32 @@
 {
-  buildPythonPackage,
+  python310Packages,
   fetchFromGitHub,
-  lib,
+  # freetype,
   ...
 }:
-buildPythonPackage {
+python310Packages.buildPythonPackage {
   pname = "dungeonsheets";
   version = "0.15.0-master";
+
+  propagatedBuildInputs = with python310Packages; [
+    sphinx
+    pdfrw
+    npyscreen
+    jinja2
+    reportlab
+    certifi
+    fdfgen
+    EBookLib
+    # (reportlab.overrideAttrs (_: {
+    #   patches = [];
+    #   buildInputs = with python310Packages; [(freetype.overrideAttrs (oldArgs: {dontDisableStatic = true;})) pillow];
+    # }))
+  ];
+
   src = fetchFromGitHub {
     owner = "canismarko";
     repo = "dungeon-sheets";
     rev = "33bffa806df356c779b8306243e48362e0e43433";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-+8or4a+LTAJq662SyAyQebgY6qafQRxBaUvS9BFjTMQ=";
   };
 }
